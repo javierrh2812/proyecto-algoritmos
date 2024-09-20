9b039db9cf2b1ab6b814e33e06f1ff80928c00a5
ELYNOR <ELYNOR@ELYNOR>
2024-09-19 20:45:55 -0500

    CAMBIOS

--- Trabajo Parcial/Guardian.h
+++ Trabajo Parcial/Guardian.h
@@ -72,6 +72,7 @@ public:
             }
             break;
         }
+<<<<<<< HEAD
 
         dibujar();
     }
@@ -117,6 +118,53 @@ public:
         return nullptr;
     }
 
+=======
+
+        dibujar();
+    }
+
+    void borrar() {
+        Console::SetCursorPosition(x, y); cout << " ";
+    }
+
+    void dibujar() {
+        Console::SetCursorPosition(x, y); cout << "@";
+    }
+
+    void recolectar(Recurso* r) {
+        if (typeid(*r) == typeid(Semilla)) {
+            this->nroSemillas++;
+        }
+        else if (typeid(*r) == typeid(Agua)) {
+            this->litrosAgua++;
+        }
+        else if (typeid(*r) == typeid(Residuo)) {
+            this->nroResiduos++;
+        }
+    }
+
+    Semilla* sembrar(char tecla) {
+        if (this->nroSemillas > 0) {
+            nroSemillas--;
+            Semilla* semilla = new Semilla(this->x + 1, this->y + 1);
+            incrementarNroArboles();
+            mostrarIndicadores();
+            return semilla;
+        }
+        return nullptr;
+    }
+
+    Agua* regar(char tecla) {
+        if (this->litrosAgua > 0) {
+            litrosAgua--;
+            Agua* agua = new Agua(this->x + 1, this->y + 1);
+            mostrarIndicadores();
+            return agua;
+        }
+        return nullptr;
+    }
+
+>>>>>>> CAMBIOS
     void disparar(char tecla) {
         if (this->nroResiduos > 0) {
             nroResiduos--;
@@ -131,6 +179,7 @@ public:
 		Console::ForegroundColor = ConsoleColor::Cyan; Console::SetCursorPosition(28, 38); cout << "AGUA ~:" << litrosAgua;
 		Console::ForegroundColor = ConsoleColor::Green; Console::SetCursorPosition(55, 38); cout << "SEMILLAS *:" << nroSemillas;
 		Console::ForegroundColor = ConsoleColor::DarkYellow; Console::SetCursorPosition(78, 38);	cout << "RESIDUOS #:" << nroResiduos;
+<<<<<<< HEAD
 	};
 };
     void mostrarIndicadores() {
@@ -139,4 +188,7 @@ public:
         Console::ForegroundColor = ConsoleColor::Green; Console::SetCursorPosition(55, 38); cout << "SEMILLAS *:" << nroSemillas;
         Console::ForegroundColor = ConsoleColor::DarkYellow; Console::SetCursorPosition(78, 38); cout << "RESIDUOS #:" << nroResiduos;
     }
+=======
+	}
+>>>>>>> CAMBIOS
 };
